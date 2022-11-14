5a7300c96b0edbde324ffc19cc2a40f2a012fcfb
k078 <77967048+k078@users.noreply.github.com>
2022-11-14 15:36:57 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './shared/app-navbar/app-navbar.component';
 import { AppFooterComponent } from './shared/app-footer/app-footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
